663ccabb15eb632112db6cc6da54f36fd65168e3
JoshSalce <65614069+joshsalce@users.noreply.github.com>
2021-10-16 20:55:16 -0700

    Update continuous_rd.R

--- continuous_rd.R
+++ continuous_rd.R
@@ -8,11 +8,9 @@ cont_RD = function(team, year) {
   d4t4 <- getRetrosheet("game", year)
   subject <- subset(d4t4, d4t4$HmTm == team | d4t4$VisTm == team)
     
-  run_diff = 0
-  initial_rd = ifelse(subject$HmTm[1] == team,  subject$HmRuns[1] - subject$VisRuns[1], subject$VisRuns[1] - subject$HmRuns[1])
   v = c(initial_rd)
     
-  for (i in 2:nrow(subject)) {
+  for (i in 1:nrow(subject)) {
     run_diff <- initial_rd + ifelse(subject$HmTm[i] == team, subject$HmRuns[i] - subject$VisRuns[i], subject$VisRuns[i] - subject$HmRuns[i]) 
     v = c(v, run_diff)
     initial_rd <- run_diff
